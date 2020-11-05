c6d243fb5771b6eec991a7e043bb476466c2e5ec
Donovan Gervais <applemac@Donovans-MacBook-Pro-6.local>
2020-11-05 15:41:32 +0000

    creating a parent class with a function that appends list

---
+++ trello_ex_1.py
@@ -0,0 +1,10 @@
+# create a parent class that has functions needed for list to 100 being outputted
+
+class FizzBuzz:
+    def __init__(self, Fizz, Buzz):
+        self.Fizz = Fizz
+        self.Bizz = Buzz
+        self.FizzBuzz = Fizz * Buzz
+
+    # create function that takes an empty list, and appends 1-100 with fizz and buzz accordingly (Check README)
+    def fizz_buzz
